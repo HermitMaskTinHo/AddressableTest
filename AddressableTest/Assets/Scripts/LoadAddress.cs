408d54b590b6a70b3eecea7d8ebcd0ec3ba7f484
TinUnity <55797283+TinUnity@users.noreply.github.com>
2020-08-27 12:39:42 +0700

    add address

---
+++ AddressableTest/Assets/Scripts/LoadAddress.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LoadAddress : MonoBehaviour
+{
+    public AssetReference m_cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_cube.InstantiateAsync(Vector2.zero,Quaternion.identity);
+    }
+}
